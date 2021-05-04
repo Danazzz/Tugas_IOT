fc8efbfc0831e19ce27fc8db0f79702c5ef11766
Danazzz <artaputra95@gmail.com>
2021-05-04 17:10:50 +0800

    menambah function getAmbientTemperature()

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,30 @@ const char* WIFI_SSID = "FTI";
 const char* WIFI_PASS = "teknikpastijaya";
 const char* HOSTNAME = "DANA";
 
+OneWire oneWire(4);
+DallasTemperature sensors(&oneWire);
+
+float getAmbientTemperature()
+{
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures();
+  Serial.println("DONE");
+
+  float tempC = sensors.getTempCByIndex(0);
+
+  if(tempC != DEVICE_DISCONNECTED_C) 
+  {
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(tempC);
+    return tempC;
+  } 
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+    return -127;
+  }
+}
+
 void setup()
 {
   Serial.begin(115200);

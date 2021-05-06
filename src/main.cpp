0210ebae9c57f09a9b6ea6ff8f8548c088b3ddec
Danazzz <artaputra95@gmail.com>
2021-05-06 23:47:27 +0800

    menambahkan comment pada tiap syntax

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,34 @@
+//include library yang diperlukan
 #include <Arduino.h>
 #include <WiFi.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+// sensor  ds18b20 disambungkan di pin 4
+#define ONE_WIRE_BUS 4
+
+//menggunakan wifi-tethering
 const char* WIFI_SSID = "FTI";
 const char* WIFI_PASS = "teknikpastijaya";
 const char* HOSTNAME = "DANA";
 
-OneWire oneWire(4);
+//berikan nama variabel untuk syntax onewire dan dallastemperature
+OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-float getAmbientTemperature()
-{
+//function untuk membaca temperature suhu
+float getAmbientTemperature(){
   Serial.print("Requesting temperatures...");
   sensors.requestTemperatures();
   Serial.println("DONE");
 
-  float tempC = sensors.getTempCByIndex(0);
-
-  if(tempC != DEVICE_DISCONNECTED_C) 
-  {
+  float tempC = sensors.getTempCByIndex(0); //temperatur celcius
+  if(tempC != DEVICE_DISCONNECTED_C){
     Serial.print("Temperature for the device 1 (index 0) is: ");
     Serial.println(tempC);
     return tempC;
   } 
-  else
-  {
+  else{
     Serial.println("Error: Could not read temperature data");
     return -127;
   }
@@ -33,8 +36,10 @@ float getAmbientTemperature()
 
 void setup()
 {
+  //menjalankan serial monitor
   Serial.begin(115200);
 
+  //koneksi ke Wifi
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   WiFi.setHostname(HOSTNAME);
@@ -46,11 +51,13 @@ void setup()
   }
   Serial.println("");
   Serial.println("WiFi connected successfully.");
-  sensors.begin();
+
+  sensors.begin(); //menjalankan sensor
 }
 
 void loop()
 {
+  //menjalankan function getAmbientTemperature()
   float suhu = getAmbientTemperature();
-  delay(3000);
+  delay(5000);
 }
